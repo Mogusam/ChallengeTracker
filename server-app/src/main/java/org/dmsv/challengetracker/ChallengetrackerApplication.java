5923cd2125094a37a0a1446168bdf3f92c357527
Viktor Peretiatko <viktor_peretiatko@epam.com>
2019-03-11 18:32:51 +0200

    Add app skeleton

---
+++ server-app/src/main/java/org/dmsv/challengetracker/ChallengetrackerApplication.java
@@ -0,0 +1,13 @@
+package org.dmsv.challengetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengetrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengetrackerApplication.class, args);
+    }
+
+}
---
+++ server-app/src/main/java/org/dmsv/challengetracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.dmsv.challengetracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChallengetrackerApplication.class);
+    }
+
+}
