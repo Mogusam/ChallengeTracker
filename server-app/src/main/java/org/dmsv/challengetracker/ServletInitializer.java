ece1746bc8529fd6e23cc4ccaa53658c0e4f7f23
Denys Sulima <Denys_Sulima@epam.com>
2019-04-03 15:40:21 +0300

    Added initial sql script to create challenge, user, challenge_template, user_challenge tables and populated challenge_template with default templates. Removed tomcat dependency from build.gradle. Changed dialect to newer version.

--- server-app/src/main/java/org/dmsv/challengetracker/ServletInitializer.java
+++ server-app/src/main/java/org/dmsv/challengetracker/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ChallengetrackerApplication.class);
+        return application.sources(ChallengeTrackerApplication.class);
     }
 
 }
