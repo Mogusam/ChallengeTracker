e1a76c541880c52bfe8de8a134e9923676057ec9
Viktor Peretiatko <viktor_peretiatko@epam.com>
2020-03-11 08:04:20 +0200

    separate ui-server-db modules.

---
+++ db/src/main/resources/db/scripts/insert_template_challenges.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.challenge_template (Template_Name, Template_Description, Template_Settings) VALUES
+('Running challenge', 'First to run 100 kilometers', '{"distance in kms": 100}'),
+('Beer mile challenge', 'Drink one beer in 12 pubs', '{"beer quantity": 12, "pubs quantity": 12}'),
+('Reading challenge', 'Read 40 books in a year', '{"books quantity": 40, "duration in years": 1}'),
+('Lose weight challenge', 'Lose 5 kg in 2 month', '{"lose weight amount": 5, "duration in month": 2}'),
+('Travel challenge', 'Visit 20 cities in one year', '{"cities quantity": 20, "duration in years": 1}');
